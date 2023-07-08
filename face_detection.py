52c141c74d96714afd840276dd85f7036f6aa073
Rola-H <82904792+Rola-H@users.noreply.github.com>
2023-07-08 20:44:19 +0300

    Update face_detection.py

--- face_detection.py
+++ face_detection.py
@@ -1,22 +1,33 @@
+from mtcnn import MTCNN
 import cv2
 
-#  face detection using haar cascades 
-def detect_faces_in_image(image_path):
-    # Load the Haar cascade classifier for face detection
-    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-
-    # Load the image
+def face_detect(image_path)
+    # Load the MTCNN face detector
+    detector = MTCNN()
+    
+    # Load and preprocess the image
     image = cv2.imread(image_path)
-
-    # Convert the image to grayscale
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-    # Perform face detection using the Haar cascade classifier
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=1)
-
-    # Draw rectangles around the detected faces
-    for (x, y, w, h) in faces:
-        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
+    
+    # Define the desired width
+    desired_width = 900
+    
+    # Calculate the aspect ratio
+    aspect_ratio = desired_width / image.shape[0]
+    
+    # Calculate the corresponding height while maintaining the aspect ratio
+    desired_height = int(image.shape[1] * aspect_ratio)
+    
+    # Resize the image
+    image = cv2.resize(image, (desired_height, desired_width))
+    
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    
+    # Perform face detection
+    faces = detector.detect_faces(image_rgb)
+    
+    # Draw rectangles on the detected faces
+    for face in faces:
+        x, y, w, h = face['box']
+        cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    
     return image
-
